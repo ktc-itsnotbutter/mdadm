38f61f1086636ca5d2ca13bf358ec12db8b67ca9
Robert <taeil.choi@kt.com>
2013-03-29 17:31:13 +0900

    fix typo

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,6 +7,6 @@ default['mdadm']['raids']['/dev/md1']['level'] = 6
 default['mdadm']['raids']['/dev/md1']['chunk'] = 64 
 default['mdadm']['raids']['/dev/md1']['devices'] = ['/dev/sdh', '/dev/sdi', '/dev/sdj', '/dev/sdk']
 
-default['mdadm']['raids']['/dev/md4']['level'] = 0 
-default['mdadm']['raids']['/dev/md1']['chunk'] = 64 
-default['mdadm']['raids']['/dev/md4']['devices'] = ['/dev/md0', '/dev/md1']
+default['mdadm']['raids']['/dev/md2']['level'] = 0 
+default['mdadm']['raids']['/dev/md2']['chunk'] = 64 
+default['mdadm']['raids']['/dev/md2']['devices'] = ['/dev/md0', '/dev/md1']
